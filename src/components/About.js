cac809ebcd3b0fe4bee2c3146e785b0221e52b39
U-DANIELS-PC\Daniel <dhb7@students.waikato.ac.nz>
2021-11-15 11:14:39 +1300

    Finished services page, mostly complete contact page

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,29 @@
 import SubHeader from "./sub/SubHeader.js";
 import Breadcrumbs from "./sub/Breadcrumbs.js";
-
+import welder from "../assets/img-welder.jpg";
 
 function About() {
 
     return (
-        <div>
-            <SubHeader title="About" />
+        <div id="about-page">
+            <SubHeader title="About Us" />
             <Breadcrumbs items={[
                 {name: "Home", link: ".."},
-                {name: "About"}
+                {name: "About Us"}
                 ]}/>
+            <div id="about-center">
+                <div id="about-left-panel">
+                    <img src={welder} alt="welder-img" className="transitionInZoomOnload"/>
+                </div>
+                <div id="about-right-panel">
+                    <h2 className="transitionInOnload">Who We Are</h2>
+                    <p className="transitionInZoomOnload">Gunabros Systems has over 15 years of manufacturing excellence, and a passion for producing high-quality precision-engineered products and services to the food industry. We have manufactured equipment for some of the biggest food providers in New Zealand including Fonterra.</p>
+                    <p className="transitionInZoomOnload">With a team of highly skilled tradesmen we aim at providing affordable, top quality solutions to our clients.</p>
+                    <p className="transitionInZoomOnload">Please feel free to contact us to provide you with consultancy and a quote for your new project, or to advise you on the best solution for your industry.</p>
+
+                    <a href="../contact">Contact Us</a>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,6 +12,12 @@ function Contact() {
                 {name: "Home", link: ".."},
                 {name: "Contact"}
                 ]}/>
+
+            <div id="contact-center" className="transitionInOnloadLong">
+                <div id="mapWrapper" className="transitionInOnloadLong">
+                    <iframe title="address" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3150.1933248144774!2d175.3269632!3d-37.8557667!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d6d1e44e6eb3f21%3A0xa7adb0d28b135ceb!2s141+Middle+Rd%2C+Rukuhia+3282!5e0!3m2!1sen!2snz!4v1418682651077" width="100%" height="400px" frameBorder="0" style={{border:0}}></iframe>
+                </div>
+            </div>
         </div>
     );
 }
